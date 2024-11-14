3bd56400d60c2cd3dadfb324ca39902331df06cd
DanielBarletta81 <dbarcelona1481@gmail.com>
2024-11-14 13:18:30 -0500

    initial upload, create app.py file

---
+++ app.py
@@ -0,0 +1,7 @@
+#Task 1: Setting Up Flask with Flask-SQLAlchemy
+#  - Initialize a new Flask project and set up a virtual environment.
+#  - Install Flask, Flask-SQLAlchemy, and Flask-Marshmallow.
+#  - Configure Flask-SQLAlchemy to connect to your database. 
+# - Define `Members` and `WorkoutSessions` models using Flask-SQLAlchemy ORM.
+
+
